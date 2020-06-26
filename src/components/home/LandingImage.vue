9c0cb435020621ba7ffaf4341d0f529037ccf06b
olivier goyon <og@combosolutions.eu>
2020-06-26 15:59:25 +0200

    le debut

---
+++ src/components/home/LandingImage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="landingimage">
+    <v-img src="http://placekitten.com/g/800/600" contain />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class LandingImage extends Vue {}
+</script>
+
+<style scoped lang="scss">
+.landingimage {
+  width: 100vw;
+  height: 80vh;
+  background-color: red;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <LandingImage />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import LandingImage from "@/components/home/LandingImage.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    LandingImage
   }
 };
 </script>
